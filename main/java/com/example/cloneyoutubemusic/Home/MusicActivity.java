3ab12f67d2bb216a553c0efd1e879f9e55443595
Kwak <128439473+Kwakyoung@users.noreply.github.com>
2023-06-30 18:12:40 +0900

    0630

---
+++ main/java/com/example/cloneyoutubemusic/Home/MusicActivity.java
@@ -0,0 +1,27 @@
+package com.example.cloneyoutubemusic.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cloneyoutubemusic.Home.DTO.HomeDTO;
+import com.example.cloneyoutubemusic.R;
+import com.example.cloneyoutubemusic.databinding.ActivityMainBinding;
+import com.example.cloneyoutubemusic.databinding.ActivityMusicBinding;
+
+public class MusicActivity extends AppCompatActivity {
+    ActivityMusicBinding binding;
+    HomeDTO dto;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMusicBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        dto = (HomeDTO) getIntent().getSerializableExtra("dto");
+
+
+
+
+    }
+}
