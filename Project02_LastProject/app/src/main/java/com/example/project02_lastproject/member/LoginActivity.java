baa574d35043eee07618ec698cffabc33736efea
Kwakyoung <128439473+Kwakyoung@users.noreply.github.com>
2023-07-17 14:03:11 +0900

    0717 소셜로그인

--- Project02_LastProject/app/src/main/java/com/example/project02_lastproject/member/LoginActivity.java
+++ Project02_LastProject/app/src/main/java/com/example/project02_lastproject/member/LoginActivity.java
@@ -1,9 +1,16 @@
 package com.example.project02_lastproject.member;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.project02_lastproject.MainActivity;
@@ -12,8 +19,26 @@ import com.example.project02_lastproject.common.CommonConn;
 import com.example.project02_lastproject.common.CommonVar;
 import com.example.project02_lastproject.databinding.ActivityLoginBinding;
 import com.google.gson.Gson;
+import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.common.KakaoSdk;
+import com.kakao.sdk.common.model.ClientErrorCause;
+import com.kakao.sdk.user.UserApiClient;
+import com.navercorp.nid.NaverIdLoginSDK;
+import com.navercorp.nid.oauth.NidOAuthLogin;
+import com.navercorp.nid.oauth.OAuthLoginCallback;
+import com.navercorp.nid.profile.NidProfileCallback;
+import com.navercorp.nid.profile.data.NidProfileResponse;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+import kotlin.jvm.functions.Function2;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // 구글 로그인은 혼자해보기. ( 구글 개발자 콘솔 등록되어야 함. )
 
     ActivityLoginBinding binding;
     @Override
@@ -21,6 +46,19 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        KakaoSdk.init(this, "1986060e76a6d35eba083bc7720aad9d");
+        NaverIdLoginSDK.INSTANCE.initialize(this, "n8t4t5Or4PjfmE_GL5Mu", "IxoQWKnmsO", getString(R.string.app_name));
+        naverLogin();
+
+
+
+
+        UserApiClient.getInstance().unlink(new Function1<Throwable, Unit>() {
+            @Override
+            public Unit invoke(Throwable throwable) {
+                return null;
+            }
+        });
 
         binding.btnLogin.setOnClickListener(v -> {
             if(binding.edtId.getText().toString().length() < 1
@@ -45,8 +83,128 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }));
         });
-
-
-
+            getHashKey();
+            binding.imgvKakaologin.setOnClickListener(v -> {
+                kakaoLogin(this);
+            });
     }
+
+    public void naverLogin(){
+        binding.buttonOAuthLoginImg.setOAuthLogin(new OAuthLoginCallback() {
+            @Override
+            public void onSuccess() {
+                Log.d("네이버", "onSuccess: "+ NaverIdLoginSDK.INSTANCE.getAccessToken());
+
+                new NidOAuthLogin().callProfileApi(new NidProfileCallback<NidProfileResponse>() {
+                    @Override
+                    public void onSuccess(NidProfileResponse res) {
+                        Log.d("네이버", "onSuccess: "+res.getProfile());
+                        Log.d("네이버", "onSuccess: "+res.getMessage());
+
+                    }
+
+                    @Override
+                    public void onFailure(int i, @NonNull String s) {
+
+                    }
+
+                    @Override
+                    public void onError(int i, @NonNull String s) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(int i, @NonNull String s) {
+                Log.d("네이버", "onFailure: "+s);
+            }
+
+            @Override
+            public void onError(int i, @NonNull String s) {
+                Log.d("네이버", "onError: "+s);
+            }
+        });
+    }
+
+    public void kakaoLogin(Context context){
+        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인 ( 웹뷰 )
+        // Kotlin은 경우에 따라서 생성자 대신에 메소드로 객체를 return받음. UserApiClient name = new ... x => UserApiClient. { static UserApiClient.method )
+
+        // ↓ 간단하게 처리한 코드
+        Function2<OAuthToken, Throwable, Unit> callback = (token, error) -> {
+            if(error != null){
+                Log.d("카카오 로그인 에러", "invoke: "+ error.getMessage());
+            }else {
+                Log.d("카카오 로그인 성공", "invoke: "+ token.toString());
+                getKakaoProfile();
+            }
+            return null;
+        };
+
+        if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(context)) {
+            Log.d("카카오", "isKakaoTalkLoginAvailable: true ");
+            UserApiClient.getInstance().loginWithKakaoTalk(context,callback);
+        }else{
+            Log.d("카카오", "isKakaoTalkLoginAvailable: false ");
+            UserApiClient.getInstance().loginWithKakaoAccount(context, callback);
+        }
+//        if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+//            UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+//                if (error != null) {
+//                    Log.e(TAG, "카카오톡으로 로그인 실패", error)
+//
+//                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+//                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+//                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+//                        return@loginWithKakaoTalk
+//                    }
+//
+//                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+//                    UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+//                } else if (token != null) {
+//                    Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+//                }
+//            }
+//        } else {
+//            UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+//        }
+    }
+
+
+    public void getKakaoProfile(){
+        UserApiClient.getInstance().me((user, error) -> {
+            if(error == null){
+                Log.d("카카오", "getKakaoProfile: "+user.toString());
+                Log.d("카카오", "getKakaoProfile: "+user.getKakaoAccount().getEmail());
+                Log.d("카카오", "getKakaoProfile: "+user.getKakaoAccount().getProfile().getNickname());
+                Log.d("카카오", "getKakaoProfile: "+user.getKakaoAccount().getProfile().getThumbnailImageUrl());
+
+            }
+            return null;
+        });
+    }
+
+
+    private void getHashKey(){
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
+    }
+
 }
